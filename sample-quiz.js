b785fe03ea4c3443cd5af93f020772809b69db16
Koji Inoue <inouekoji2013@gmail.com>
2019-10-08 12:54:23 +0900

    Updated

--- sample-quiz.js
+++ sample-quiz.js
@@ -4,11 +4,11 @@
  
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
+//const {V2Agent} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
 
-// クイズとヒントの定義
 const quiz = [
-	{ one : "私は黄色いです。", two : "私は細長いです。", three : "私はくだものです。", answer : "バナナ"},
+    { one : "私は黄色いです。", two : "私は細長いです。", three : "私はくだものです。", answer : "バナナ"},
   　{ one : "私は川です。", two : "私は東北地方にいます。", three : "私は日本一長い川です。", answer : "信濃川"}
 ];
  
@@ -18,58 +18,60 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-  
+ 
   function start(agent) {
-	// ランダムにクイズを選択
-	var quiz_index = Math.floor( Math.random() * 2 );
-	let quiz_selected = quiz[quiz_index];
-	agent.add('クイズです。私は誰でしょう？' + quiz_selected.one);
-	agent.setContext({ name: 'quiz_context', lifespan: 1, parameters: { time: 1, quiz_index: quiz_index, answer: quiz_selected.answer}});
+    var quiz_index = Math.floor( Math.random() * 2 );
+    let quiz_selected = quiz[quiz_index];
+    agent.add('クイズです。私は誰でしょう？' + quiz_selected.one);
+    agent.setContext({ name: 'quiz_context', lifespan: 1, parameters: { time: 1, quiz_index: quiz_index, answer: quiz_selected.answer}});
+    agent.clearContext('end');
   }
   
   function answer(agent){
+    
+    // ユーザの回答を取得
+    var answer_user = request.body.queryResult.queryText;
 	
-	// ユーザの回答を取得
-	var answer_user = request.body.queryResult.queryText;
-	
-	// 答えなどを文脈から取得
-	var context = agent.getContext('quiz_context');
-	var answer_ref = context.parameters.answer;
-	
-	if (context){
-		agent.add('もう一度やる場合は、もう一度、と言ってください。');
-		return;
-	}
-	
-	if (answer_user.indexOf(answer_ref) != -1){
-	  //正解
-	  agent.add('正解です。おめでとうございます。');
-	  agent.setContext({ name: 'end', lifespan: 1});
-	}
-	else{
-	  // 不正解
-	  var time = context.parameters.time;
-	  var quiz_index = context.parameters.quiz_index;
-	  let quiz_selected = quiz[quiz_index];
-	  
-	  if (time == 3){
-		// 終了
-		agent.add('残念。答えは' + answer_ref + 'です。また遊びましょう。');
-		agent.setContext({ name: 'end', lifespan: 1});
-		return;
-	  }
-	  else if (time == 2){
-	  	// 3回目のヒント
-		let next_hint = quiz_selected.three;
-		agent.add('違います。最後のヒントは、' + next_hint);
-	  }
-	  else if (time == 1){
-	  	// 2回目のヒント
-		let next_hint = quiz_selected.two;
-		agent.add('違います。次のヒントは、' + next_hint);
-	  }
-	  agent.setContext({ name: 'quiz_context', lifespan: 1, parameters: { time: time+1, quiz_index: quiz_index, answer: answer_ref}});
-	}
+    // 答えなどを文脈から取得
+    var context = agent.getContext('quiz_context');
+    var context_end = agent.getContext('end');
+    
+    if (context_end){
+    	agent.add('もう一度やる場合は、もう一度、と言ってください。');
+        return;
+    }
+    
+    var answer_ref = context.parameters.answer;
+    
+    if (answer_user.indexOf(answer_ref) != -1){
+      //正解
+      agent.add('正解です。おめでとうございます。');
+      agent.setContext({ name: 'end', lifespan: 100});
+    }
+    else{
+      // 不正解
+      var time = context.parameters.time;
+      var quiz_index = context.parameters.quiz_index;
+      let quiz_selected = quiz[quiz_index];
+      
+      if (time == 3){
+        // 終了
+        agent.add('残念。答えは' + answer_ref + 'です。また遊びましょう。');
+        agent.setContext({ name: 'end', lifespan: 100});
+        return;
+      }
+      else if (time == 2){
+      	// 3回目のヒント
+        let next_hint = quiz_selected.three;
+        agent.add('違います。最後のヒントは、' + next_hint);
+      }
+      else if (time == 1){
+      	// 2回目のヒント
+        let next_hint = quiz_selected.two;
+        agent.add('違います。次のヒントは、' + next_hint);
+      }
+      agent.setContext({ name: 'quiz_context', lifespan: 1, parameters: { time: time+1, quiz_index: quiz_index, answer: answer_ref}});
+    }
   }
 
   // Run the proper function handler based on the matched Dialogflow intent name
